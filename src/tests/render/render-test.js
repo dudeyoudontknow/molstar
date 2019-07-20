390359efc689390501ff7b26a84f8aeaf1ef37e0
Jesse Liang <jesseliang@ymail.com>
2019-07-19 18:45:34 -0700

    finished testing for larger render

--- src/tests/render/render-test.js
+++ src/tests/render/render-test.js
@@ -3,6 +3,7 @@
  * Copyright (c) 2019 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ * @author Jesse Liang <jesse.liang@rcsb.org>
  */
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
@@ -51,20 +52,34 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+//import * as argparse from 'argparse'
+var pixelmatch = require("pixelmatch");
+var createContext = require("gl");
 var fs = require("fs");
 var pngjs_1 = require("pngjs");
-var createContext = require("gl");
 var canvas3d_1 = require("../../mol-canvas3d/canvas3d");
 var input_observer_1 = require("../../mol-util/input/input-observer");
-var tables_1 = require("../../mol-util/color/tables");
-var cif_1 = require("../../mol-io/reader/cif");
-var structure_1 = require("../../mol-model/structure");
 var color_1 = require("../../mol-theme/color");
 var size_1 = require("../../mol-theme/size");
 var cartoon_1 = require("../../mol-repr/structure/representation/cartoon");
+var molecular_surface_1 = require("../../mol-repr/structure/representation/molecular-surface");
+var ball_and_stick_1 = require("../../mol-repr/structure/representation/ball-and-stick");
+var gaussian_surface_1 = require("../../mol-repr/structure/representation/gaussian-surface");
 var mmcif_1 = require("../../mol-model-formats/structure/mmcif");
-var width = 2048;
-var height = 1536;
+var structure_1 = require("../../mol-model/structure");
+var tables_1 = require("../../mol-util/color/tables");
+var model_1 = require("../../apps/structure-info/model");
+var cifList = Array(); // List of cif's to render and test
+cifList = ['1crn'];
+var show = {
+    cartoon: true,
+    ballAndStick: false,
+    molecularSurface: false,
+    gaussianSurface: false,
+};
+var width = 320;
+var height = 320;
+var IMAGE_PATH = 'images/';
 var gl = createContext(width, height, {
     alpha: false,
     antialias: true,
@@ -81,6 +96,23 @@ var canvas3d = canvas3d_1.Canvas3D.create(gl, input, {
     postprocessing: __assign({}, canvas3d_1.Canvas3DParams.postprocessing.defaultValue, { occlusionEnable: true, outlineEnable: true })
 });
 canvas3d.animate();
+var reprCtx = {
+    wegbl: canvas3d.webgl,
+    colorThemeRegistry: color_1.ColorTheme.createRegistry(),
+    sizeThemeRegistry: size_1.SizeTheme.createRegistry()
+};
+function getCartoonRepr() {
+    return cartoon_1.CartoonRepresentationProvider.factory(reprCtx, cartoon_1.CartoonRepresentationProvider.getParams);
+}
+function getBallAndStickRepr() {
+    return ball_and_stick_1.BallAndStickRepresentationProvider.factory(reprCtx, ball_and_stick_1.BallAndStickRepresentationProvider.getParams);
+}
+function getMolecularSurfaceRepr() {
+    return molecular_surface_1.MolecularSurfaceRepresentationProvider.factory(reprCtx, molecular_surface_1.MolecularSurfaceRepresentationProvider.getParams);
+}
+function getGaussianSurfaceRepr() {
+    return gaussian_surface_1.GaussianSurfaceRepresentationProvider.factory(reprCtx, gaussian_surface_1.GaussianSurfaceRepresentationProvider.getParams);
+}
 function getModels(frame) {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
@@ -98,88 +130,133 @@ function getStructure(model) {
         });
     });
 }
-function parseCif(data) {
-    return __awaiter(this, void 0, void 0, function () {
-        var comp, parsed;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    comp = cif_1.CIF.parse(data);
-                    return [4 /*yield*/, comp.run()];
-                case 1:
-                    parsed = _a.sent();
-                    console.log('\n\n\n' + parsed + '\n\n\n');
-                    if (parsed.isError)
-                        throw parsed;
-                    console.log('after?');
-                    return [2 /*return*/, parsed.result];
-            }
-        });
-    });
-}
-var reprCtx = {
-    colorThemeRegistry: color_1.ColorTheme.createRegistry(),
-    sizeThemeRegistry: size_1.SizeTheme.createRegistry()
-};
-function getCartoonRepr() {
-    return cartoon_1.CartoonRepresentationProvider.factory(reprCtx, cartoon_1.CartoonRepresentationProvider.getParams);
-}
-function init() {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            fs.readFile("../../../examples/1crn.cif", function (err, data) {
-                return __awaiter(this, void 0, void 0, function () {
-                    var parsed, cif, models, structure, cartoonRepr, e_1;
-                    return __generator(this, function (_a) {
-                        switch (_a.label) {
-                            case 0:
-                                if (err)
-                                    throw err;
-                                _a.label = 1;
-                            case 1:
-                                _a.trys.push([1, 6, , 7]);
-                                return [4 /*yield*/, parseCif(data)];
-                            case 2:
-                                parsed = _a.sent();
-                                cif = parsed.blocks[0];
-                                return [4 /*yield*/, getModels(cif)];
-                            case 3:
-                                models = _a.sent();
-                                return [4 /*yield*/, getStructure(models[0])];
-                            case 4:
-                                structure = _a.sent();
-                                cartoonRepr = getCartoonRepr();
-                                cartoonRepr.setTheme({
-                                    color: reprCtx.colorThemeRegistry.create('sequence-id', { structure: structure }),
-                                    size: reprCtx.sizeThemeRegistry.create('uniform', { structure: structure })
-                                });
-                                return [4 /*yield*/, cartoonRepr.createOrUpdate(__assign({}, cartoon_1.CartoonRepresentationProvider.defaultValues, { quality: 'auto' }), structure).run()];
-                            case 5:
-                                _a.sent();
-                                canvas3d.add(cartoonRepr);
-                                canvas3d.resetCamera();
-                                return [3 /*break*/, 7];
-                            case 6:
-                                e_1 = _a.sent();
-                                console.error(e_1);
-                                process.exit(1);
-                                return [3 /*break*/, 7];
-                            case 7: return [2 /*return*/];
-                        }
-                    });
-                });
+function runTests() {
+    cifList.forEach(function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            var cif, models, structure, cartoonRepr, ballAndStickRepr, molecularSurfaceRepr, gaussianSurfaceRepr, e_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 12, , 13]);
+                        return [4 /*yield*/, model_1.readCifFile('../../../examples/' + id + '.cif')]; // Similar to render-structure, but accesses local cif
+                    case 1:
+                        cif = _a.sent() // Similar to render-structure, but accesses local cif
+                        ;
+                        return [4 /*yield*/, getModels(cif)];
+                    case 2:
+                        models = _a.sent();
+                        return [4 /*yield*/, getStructure(models[0])];
+                    case 3:
+                        structure = _a.sent();
+                        cartoonRepr = getCartoonRepr();
+                        ballAndStickRepr = getBallAndStickRepr();
+                        molecularSurfaceRepr = getMolecularSurfaceRepr();
+                        gaussianSurfaceRepr = getGaussianSurfaceRepr();
+                        if (!show.cartoon) return [3 /*break*/, 5];
+                        cartoonRepr.setTheme({
+                            color: reprCtx.colorThemeRegistry.create('sequence-id', { structure: structure }),
+                            size: reprCtx.sizeThemeRegistry.create('uniform', { structure: structure })
+                        });
+                        return [4 /*yield*/, cartoonRepr.createOrUpdate(__assign({}, cartoon_1.CartoonRepresentationProvider.defaultValues, { quality: 'auto' }), structure).run()];
+                    case 4:
+                        _a.sent();
+                        _a.label = 5;
+                    case 5:
+                        if (!show.ballAndStick) return [3 /*break*/, 7];
+                        ballAndStickRepr.setTheme({
+                            color: reprCtx.colorThemeRegistry.create('sequence-id', { structure: structure }),
+                            size: reprCtx.sizeThemeRegistry.create('uniform', { structure: structure })
+                        });
+                        return [4 /*yield*/, ballAndStickRepr.createOrUpdate(__assign({}, ball_and_stick_1.BallAndStickRepresentationProvider.defaultValues, { quality: 'auto' }), structure).run()];
+                    case 6:
+                        _a.sent();
+                        _a.label = 7;
+                    case 7:
+                        if (!show.molecularSurface) return [3 /*break*/, 9];
+                        molecularSurfaceRepr.setTheme({
+                            color: reprCtx.colorThemeRegistry.create('sequence-id', { structure: structure }),
+                            size: reprCtx.sizeThemeRegistry.create('uniform', { structure: structure })
+                        });
+                        console.time('molecular surface');
+                        return [4 /*yield*/, molecularSurfaceRepr.createOrUpdate(__assign({}, molecular_surface_1.MolecularSurfaceRepresentationProvider.defaultValues, { quality: 'custom', alpha: 0.5, flatShaded: true, doubleSided: true, resolution: 0.3 }), structure).run()];
+                    case 8:
+                        _a.sent();
+                        console.timeEnd('molecular surface');
+                        _a.label = 9;
+                    case 9:
+                        if (!show.gaussianSurface) return [3 /*break*/, 11];
+                        gaussianSurfaceRepr.setTheme({
+                            color: reprCtx.colorThemeRegistry.create('sequence-id', { structure: structure }),
+                            size: reprCtx.sizeThemeRegistry.create('uniform', { structure: structure })
+                        });
+                        console.time('gaussian surface');
+                        return [4 /*yield*/, gaussianSurfaceRepr.createOrUpdate(__assign({}, gaussian_surface_1.GaussianSurfaceRepresentationProvider.defaultValues, { quality: 'custom', alpha: 1.0, flatShaded: true, doubleSided: true, resolution: 0.3 }), structure).run()];
+                    case 10:
+                        _a.sent();
+                        console.timeEnd('gaussian surface');
+                        _a.label = 11;
+                    case 11:
+                        if (show.cartoon)
+                            canvas3d.add(cartoonRepr);
+                        if (show.ballAndStick)
+                            canvas3d.add(ballAndStickRepr);
+                        if (show.molecularSurface)
+                            canvas3d.add(molecularSurfaceRepr);
+                        if (show.gaussianSurface)
+                            canvas3d.add(gaussianSurfaceRepr);
+                        canvas3d.resetCamera();
+                        return [3 /*break*/, 13];
+                    case 12:
+                        e_1 = _a.sent();
+                        console.error(e_1);
+                        process.exit(1);
+                        return [3 /*break*/, 13];
+                    case 13:
+                        setTimeout(function () {
+                            process.stdout.write('Testing render of ' + id + ': ');
+                            var pixelData = canvas3d.getPixelData('color');
+                            var generatedPng = new pngjs_1.PNG({ width: width, height: height });
+                            generatedPng.data = Buffer.from(pixelData.array);
+                            var numDiffPx = 1;
+                            try {
+                                var baselinePng = pngjs_1.PNG.sync.read(fs.readFileSync(IMAGE_PATH + 'baseline' + id + '.png'));
+                                var diff = new pngjs_1.PNG({ width: width, height: height });
+                                numDiffPx = pixelmatch(baselinePng.data, generatedPng.data, diff.data, baselinePng.width, baselinePng.height, { threshold: 0 });
+                                fs.writeFileSync(IMAGE_PATH + 'differences.png', pngjs_1.PNG.sync.write(diff));
+                            }
+                            catch (e) {
+                                console.log('\x1b[32m%s\x1b[0m', 'FAILED');
+                            }
+                            if (numDiffPx > 0) {
+                                console.log('\x1b[31m%s\x1b[0m', 'FAILED');
+                            }
+                            else {
+                                console.log('\x1b[32m%s\x1b[0m', 'PASSED');
+                            }
+                            generatedPng.pack().pipe(fs.createWriteStream(IMAGE_PATH + 'output' + id + '.png')).on('finish', function () {
+                                process.exit();
+                            });
+                        }, 500);
+                        return [2 /*return*/];
+                }
             });
-            // canvas3d.setProps({ trackball: { ...canvas3d.props.trackball, spin: true } })
-            setTimeout(function () {
-                var pixelData = canvas3d.getPixelData('color');
-                var png = new pngjs_1.PNG({ width: width, height: height });
-                png.data = Buffer.from(pixelData.array);
-                png.pack().pipe(fs.createWriteStream('output.png')).on('finish', function () {
-                    process.exit();
-                });
-            }, 500);
-            return [2 /*return*/];
         });
     });
 }
-init();
+// TODO: implement argparse
+// const parser = new argparse.ArgumentParser({
+//     addHelp: true,
+//     description: 'render image as PNG (work in progress)'
+// });
+// parser.addArgument([ 'id' ], {
+//     help: 'PDB ID'
+// });
+// parser.addArgument([ 'out' ], {
+//     help: 'image output path'
+// });
+// interface Args {
+//     id: string
+//     out: string
+// }
+// const args: Args = parser.parseArgs();
+runTests();
--- src/tests/render/render-test.ts
+++ src/tests/render/render-test.ts
@@ -2,9 +2,11 @@
  * Copyright (c) 2019 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ * @author Jesse Liang <jesse.liang@rcsb.org>
  */
 
-import * as argparse from 'argparse'
+//import * as argparse from 'argparse'
+import pixelmatch = require('pixelmatch')
 import createContext = require('gl')
 import fs = require('fs')
 import { PNG } from 'pngjs'
@@ -13,14 +15,28 @@ import InputObserver from '../../mol-util/input/input-observer';
 import { ColorTheme } from '../../mol-theme/color';
 import { SizeTheme } from '../../mol-theme/size';
 import { CartoonRepresentationProvider } from '../../mol-repr/structure/representation/cartoon';
+import { MolecularSurfaceRepresentationProvider } from '../../mol-repr/structure/representation/molecular-surface';
+import { BallAndStickRepresentationProvider } from '../../mol-repr/structure/representation/ball-and-stick';
+import { GaussianSurfaceRepresentationProvider } from '../../mol-repr/structure/representation/gaussian-surface';
 import { CifFrame } from '../../mol-io/reader/cif'
 import { trajectoryFromMmCIF } from '../../mol-model-formats/structure/mmcif';
 import { Model, Structure } from '../../mol-model/structure';
 import { ColorNames } from '../../mol-util/color/tables';
 import { readCifFile } from '../../apps/structure-info/model';
 
+let cifList = Array<string>(); // List of cif's to render and test
+cifList = ['1crn']
+const show = { // Which representations to show
+    cartoon: true,
+    ballAndStick: false,
+    molecularSurface: false,
+    gaussianSurface: false,
+}
+
 const width = 320
 const height = 320
+const IMAGE_PATH = 'images/'
+
 const gl = createContext(width, height, {
     alpha: false,
     antialias: true,
@@ -57,6 +73,18 @@ function getCartoonRepr() {
     return CartoonRepresentationProvider.factory(reprCtx, CartoonRepresentationProvider.getParams)
 }
 
+function getBallAndStickRepr() {
+    return BallAndStickRepresentationProvider.factory(reprCtx, BallAndStickRepresentationProvider.getParams)
+}
+
+function getMolecularSurfaceRepr() {
+    return MolecularSurfaceRepresentationProvider.factory(reprCtx, MolecularSurfaceRepresentationProvider.getParams)
+}
+
+function getGaussianSurfaceRepr() {
+    return GaussianSurfaceRepresentationProvider.factory(reprCtx, GaussianSurfaceRepresentationProvider.getParams)
+}
+
 async function getModels(frame: CifFrame) {
     return await trajectoryFromMmCIF(frame).run();
 }
@@ -65,38 +93,94 @@ async function getStructure(model: Model) {
     return Structure.ofModel(model);
 }
 
-async function run() {
-    try {
-        const cif = await readCifFile('../../../examples/1crn.cif')
-        const models = await getModels(cif as CifFrame)
-        const structure = await getStructure(models[0])
-
-        const cartoonRepr = getCartoonRepr()
-        cartoonRepr.setTheme({
-            color: reprCtx.colorThemeRegistry.create('sequence-id', { structure }),
-            size: reprCtx.sizeThemeRegistry.create('uniform', { structure })
-        })
-        await cartoonRepr.createOrUpdate({ ...CartoonRepresentationProvider.defaultValues, quality: 'auto' }, structure).run()
-
-        canvas3d.add(cartoonRepr)
-        canvas3d.resetCamera()
-    } catch (e) {
-        console.error(e)
-        process.exit(1)
-    }
-
-    setTimeout(() => {
-        const pixelData = canvas3d.getPixelData('color')
-        const png = new PNG({ width, height })
-        png.data = Buffer.from(pixelData.array)
-        png.pack().pipe(fs.createWriteStream('output2.png')).on('finish', () => {
-            process.exit()
-        })
-    }, 500)
+function runTests() {
+    cifList.forEach(async function(id) {
+        try {
+            const cif = await readCifFile('../../../examples/' + id + '.cif') // Similar to render-structure, but accesses local cif
+            const models = await getModels(cif as CifFrame)
+            const structure = await getStructure(models[0])
+    
+            const cartoonRepr = getCartoonRepr()
+            const ballAndStickRepr = getBallAndStickRepr()
+            const molecularSurfaceRepr = getMolecularSurfaceRepr()
+            const gaussianSurfaceRepr = getGaussianSurfaceRepr()
+        
+            if (show.cartoon) {
+                cartoonRepr.setTheme({
+                    color: reprCtx.colorThemeRegistry.create('sequence-id', { structure }),
+                    size: reprCtx.sizeThemeRegistry.create('uniform', { structure })
+                })
+                await cartoonRepr.createOrUpdate({ ...CartoonRepresentationProvider.defaultValues, quality: 'auto' }, structure).run()
+            }
+        
+            if (show.ballAndStick) {
+                ballAndStickRepr.setTheme({
+                    color: reprCtx.colorThemeRegistry.create('sequence-id', { structure }),
+                    size: reprCtx.sizeThemeRegistry.create('uniform', { structure })
+                })
+                await ballAndStickRepr.createOrUpdate({ ...BallAndStickRepresentationProvider.defaultValues, quality: 'auto' }, structure).run()
+            }
+        
+            if (show.molecularSurface) {
+                molecularSurfaceRepr.setTheme({
+                    color: reprCtx.colorThemeRegistry.create('sequence-id', { structure }),
+                    size: reprCtx.sizeThemeRegistry.create('uniform', { structure })
+                })
+                console.time('molecular surface')
+                await molecularSurfaceRepr.createOrUpdate({ ...MolecularSurfaceRepresentationProvider.defaultValues, quality: 'custom', alpha: 0.5, flatShaded: true, doubleSided: true, resolution: 0.3 }, structure).run()
+                console.timeEnd('molecular surface')
+            }
+        
+            if (show.gaussianSurface) {
+                gaussianSurfaceRepr.setTheme({
+                    color: reprCtx.colorThemeRegistry.create('sequence-id', { structure }),
+                    size: reprCtx.sizeThemeRegistry.create('uniform', { structure })
+                })
+                console.time('gaussian surface')
+                await gaussianSurfaceRepr.createOrUpdate({ ...GaussianSurfaceRepresentationProvider.defaultValues, quality: 'custom', alpha: 1.0, flatShaded: true, doubleSided: true, resolution: 0.3 }, structure).run()
+                console.timeEnd('gaussian surface')
+            }
+        
+            if (show.cartoon) canvas3d.add(cartoonRepr)
+            if (show.ballAndStick) canvas3d.add(ballAndStickRepr)
+            if (show.molecularSurface) canvas3d.add(molecularSurfaceRepr)
+            if (show.gaussianSurface) canvas3d.add(gaussianSurfaceRepr)
+            canvas3d.resetCamera()
+        } catch (e) {
+            console.error(e)
+            process.exit(1)
+        }
+    
+        setTimeout(() => {
+            process.stdout.write('Testing render of ' + id + ': ');
+            const pixelData = canvas3d.getPixelData('color')
+            const generatedPng = new PNG({ width, height })
+            generatedPng.data = Buffer.from(pixelData.array)
+            let numDiffPx = 1;
+            try {
+                const baselinePng = PNG.sync.read(fs.readFileSync(IMAGE_PATH + 'baseline' + id + '.png'))
+                const diff = new PNG({width, height})
+                numDiffPx = pixelmatch(baselinePng.data, generatedPng.data, diff.data, baselinePng.width, baselinePng.height,
+                    {threshold: 0})
+                fs.writeFileSync(IMAGE_PATH + 'differences.png', PNG.sync.write(diff));
+            } catch (e) {
+                console.log('\x1b[31m%s\x1b[0m', 'FAILED');
+            }
+            
+            if (numDiffPx > 0) {
+                console.log('\x1b[31m%s\x1b[0m', 'FAILED');
+            } else {
+                console.log('\x1b[32m%s\x1b[0m', 'PASSED');
+            }
+    
+            generatedPng.pack().pipe(fs.createWriteStream(IMAGE_PATH + 'output' + id + '.png')).on('finish', () => {
+                process.exit()
+            })
+        }, 500)
+    })
 }
 
-//
-
+// TODO: implement argparse
 // const parser = new argparse.ArgumentParser({
 //     addHelp: true,
 //     description: 'render image as PNG (work in progress)'
@@ -114,4 +198,4 @@ async function run() {
 // }
 // const args: Args = parser.parseArgs();
 
-run()
+runTests()
